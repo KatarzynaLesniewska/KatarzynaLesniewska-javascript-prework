0acdb61cb7769c6369cc4543d355feab12651012
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-06 19:43:28 +0200

    Add else if 'papier', 'nożyce'

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+} else if(randomNumber == 2){
+    computerMove = 'papier';
+} else if(randomNumber == 3){
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
