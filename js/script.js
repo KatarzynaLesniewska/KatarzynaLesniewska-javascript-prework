61e1b8b32579e08b7b6c828fc6327a0126f9d77f
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-15 18:11:37 +0200

    Put document. outside brackets

--- js/script.js
+++ js/script.js
@@ -56,19 +56,18 @@ function playGame(playerInput) {
     console.log('Gracz wpisał: ' + playerMove);
     
     displayResult(argComputerMove, argPlayerMove)
+} 
 
-    document.getElementById('play-rock').addEventListener('click', function () {
-        playGame(1);
-    });
+document.getElementById('play-rock').addEventListener('click', function () {
+    playGame(1);
+});
 
-    document.getElementById('play-paper').addEventListener('click', function () {
-        playGame(2);
-    });
+document.getElementById('play-paper').addEventListener('click', function () {
+    playGame(2);
+});
 
-    document.getElementById('play-scissors').addEventListener('click', function () {
-        playGame(3);
-    });
-
-}
+document.getElementById('play-scissors').addEventListener('click', function () {
+    playGame(3);
+});
 
 // playGame(3);
