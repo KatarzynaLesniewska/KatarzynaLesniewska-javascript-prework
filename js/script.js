42c4b81aefe587fd586ed9a10b12e78e71584319
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-15 18:47:32 +0200

    Change named functions to anonymous

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 {
-    function playGame(playerInput) {
+    const playGame = function (playerInput) {
         clearMessages();
-    
-        function getMoveName(argMoveId) {
+
+        const getMoveName = function (argMoveId) {
             console.log('wywołanie funkcji getMoveId - sprawdzenie działania');
             console.log('argumenty:' + argMoveId);
             console.log('argumenty:', argMoveId);
-    
+
             if (argMoveId == 1) {
                 return 'kamień';
             } else if (argMoveId == 2) {
                 return 'papier';
             } else if (argMoveId == 3) {
                 return 'nożyce';
-            } else { 
+            } else {
                 printMessage('Nie znam ruchu o id ' + argMoveId + '.');
                 return 'nieznany ruch';
             }
-        
-        } 
-    
-        function displayResult(argComputerMove, argPlayerMove) {
+
+        }
+
+        const displayResult = function (argComputerMove, argPlayerMove) {
             console.log('moves:', argComputerMove, argPlayerMove);
             console.log('argumenty: ' + argComputerMove + ',' + argPlayerMove);
-            printMessage('Twój ruch to: ' + argPlayerMove); 
-    
+            printMessage('Twój ruch to: ' + argPlayerMove);
+
             if (argPlayerMove == argComputerMove) {
                 printMessage('REMIS!');
             } else if (
@@ -40,36 +40,36 @@
                 printMessage('Przegrywasz!');
             }
         }
-    
+
         let randomNumber = Math.floor(Math.random() * 3 + 1);
-    
+
         console.log('Wylosowana liczba to: ' + randomNumber);
         console.log('Wylosowana liczba to: ', randomNumber);
-    
+
         let computerMove = getMoveName(randomNumber);
-        
+
         printMessage('Mój ruch to: ' + computerMove);
-    
+
         let playerMove = getMoveName(playerInput);
-    
+
         console.log(getMoveName('2')); // czy to tu ma być ? ; 
-    
+
         console.log('Gracz wpisał: ' + playerMove);
-        
+
         displayResult(argComputerMove, argPlayerMove)
-    } 
-    
+    }
+
     document.getElementById('play-rock').addEventListener('click', function () {
         playGame(1);
     });
-    
+
     document.getElementById('play-paper').addEventListener('click', function () {
         playGame(2);
     });
-    
+
     document.getElementById('play-scissors').addEventListener('click', function () {
         playGame(3);
     });
-    
+
     // playGame(3);
 } 
